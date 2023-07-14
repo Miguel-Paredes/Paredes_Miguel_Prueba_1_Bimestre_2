d2d0645d6fd505e29021139cdbcf8b9203f3da1a
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-14 11:36:44 -0500

    update

--- src/Prueba_Bimestre_II.java
+++ src/Prueba_Bimestre_II.java
@@ -8,19 +8,19 @@ public class Prueba_Bimestre_II {
     private JLabel nombre;
     private JLabel apellido;
     private JLabel signo;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField es_cod;
+    private JTextField es_ced;
+    private JTextField es_nom;
+    private JTextField es_apel;
     private JLabel nacimiento;
     private JLabel color;
     private JLabel casado;
     private JCheckBox rojo;
     private JCheckBox verde;
-    private JComboBox año;
+    private JComboBox anio;
     private JCheckBox niguno;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JComboBox mes;
+    private JComboBox dia;
     private JRadioButton si;
     private JRadioButton no;
     private JComboBox zigno;
@@ -37,4 +37,5 @@ public class Prueba_Bimestre_II {
         frame.pack();
         frame.setVisible(true);
     }
+
 }
